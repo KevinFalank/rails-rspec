f4f050cc9756b7e9ad27ce7533cc596e38178128
Apprentice <junting.lu@gmail.com>
2014-02-18 18:50:20 -0600

    Admin spec rspec tests passing

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -1,12 +1,32 @@
 require 'spec_helper'
 
 feature 'Admin panel' do
+
+  before(:each) do
+    page.driver.browser.authorize "geek", "jock"
+  end
+
   context "on admin homepage" do
-    it "can see a list of recent posts"
+    it "can see a list of recent posts" do
+      post = Post.new(title: "more postings", content: "More fancy things")
+      post.save
+      visit admin_posts_url
+      page.should have_content "More Postings"
+    end
 
-    it "can edit a post by clicking the edit link next to a post"
+    it "can edit a post by clicking the edit link next to a post" do
+      post = Post.create(title: "more postings", content: "More fancy things")
+      visit admin_posts_url
+      click_link "Edit"
+      expect(page).to have_content "Content:"
+    end
 
-    it "can delete a post by clicking the delete link next to a post"
+    it "can delete a post by clicking the delete link next to a post" do
+      post = Post.create(title: "more postings", content: "More fancy things")
+      visit admin_posts_url
+      click_link "Delete"
+      expect(page).to have_no_content("More Postings")
+    end
 
     it "can create a new post and view it" do
        visit new_admin_post_url
@@ -25,17 +45,35 @@ feature 'Admin panel' do
 
   context "editing post" do
     it "can mark an existing post as unpublished" do
-      pending # remove this line when you're working on implementing this test
-
+      post = Post.create(title: "more postings", content: "More fancy things", is_published: true)
+      visit admin_posts_url
+      click_link "Edit"
+      page.uncheck("post[is_published]")
+      click_button "Save"
       page.should have_content "Published: false"
     end
   end
 
   context "on post show page" do
-    it "can visit a post show page by clicking the title"
+    it "can visit a post show page by clicking the title" do
+      #can't be done!
+      post = Post.create(title: "more postings", content: "More fancy things", is_published: true)
+      visit admin_posts_url
+      click_link "More Postings"
+      page.should have_content "More Postings"
+    end
 
-    it "can see an edit link that takes you to the edit post path"
+    it "can see an edit link that takes you to the edit post path" do
+      post = Post.create(title: "more postings", content: "More fancy things", is_published: true)
+      visit admin_post_url(post.id)
+      page.should have_content "Edit"
+    end
 
-    it "can go to the admin homepage by clicking the Admin welcome page link"
+    it "can go to the admin homepage by clicking the Admin welcome page link" do
+      post = Post.create(title: "more postings", content: "More fancy things", is_published: true)
+      visit admin_post_url(post.id)
+      click_link "Admin welcome page"
+      page.should have_content "Welcome to the admin panel!"
+    end
   end
 end
