435b0c7696f250933c1ecef4ae08a7e3da748559
Apprentice <junting.lu@gmail.com>
2014-02-18 19:05:12 -0600

    user feature rspec passing

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,29 +1,30 @@
 require 'spec_helper'
 
 feature 'User browsing the website' do
+
+  before(:each) do
+    Post.create(title: "Hello", content: "I'm here!")
+    Post.create(title: "Hola", content: "Yo estoy aqui!")
+  end
+
   context "on homepage" do
     it "sees a list of recent posts titles" do
-      pending
-      # given a user and a list of posts
-      # user visits the homepage
-      # user can see the posts titles
+      visit root_url
+      page.should have_content "Hola"
     end
 
     it "can click on titles of recent posts and should be on the post show page" do
-      pending
-      # given a user and a list of posts
-      # user visits the homepage
-      # when a user can clicks on a post title they should be on the post show page
+      visit root_url
+      click_link "Hello"
+      page.should have_content "I'm here!"
     end
   end
 
   context "post show page" do
     it "sees title and body of the post" do
-      pending
-      # given a user and post(s)
-      # user visits the post show page
-      # user should see the post title
-      # user should see the post body
+      visit root_url
+      click_link "Hola"
+      page.should have_content "Hola Yo estoy aqui!"
     end
   end
 end
